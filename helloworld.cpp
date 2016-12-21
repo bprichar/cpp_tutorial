bbcd61a64ce9c62ebf0c6045735ee3e67f94dba3
Brad Richardson <brichardson@structint.com>
2016-12-21 15:33:06 -0800

    Make changes for second program

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,7 +1,8 @@
-// my first program in C++
+// my second program in C++
 #include <iostream>
 
 int main()
 {
-    std::cout << "Helllo World!";
+    std::cout << "Helllo World! ";
+    std::cout << "I'm a C++ program";
 }
