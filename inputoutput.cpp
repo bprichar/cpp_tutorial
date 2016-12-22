f8d58108ff59d01614e58be06f2443aace58f11a
Brad Richardson <brichardson@structint.com>
2016-12-22 08:16:19 -0800

    More playing with i/o

--- inputoutput.cpp
+++ inputoutput.cpp
@@ -1,14 +1,16 @@
-// i/o example
-
+// cin with strings
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main ()
 {
-    int i;
-    cout << "Please enter an integer value: ";
-    cin >> i;
-    cout << "The value you entered is " << i;
-    cout << " and double is " << i * 2 << ".\n";
+    string mystr;
+    cout << "What's your name? ";
+    getline(cin, mystr);
+    cout << "Hello " << mystr << ".\n";
+    cout << "What is your favorite team? ";
+    getline(cin, mystr);
+    cout << "I like " << mystr << " too!\n";
     return 0;
 }
