aa9f681a584c562046b24cc86abaa468b4bd2c3b
Brad Richardson <brichardson@structint.com>
2016-12-22 07:48:08 -0800

    More playing with strings

--- strings.cpp
+++ strings.cpp
@@ -6,7 +6,9 @@ using namespace std;
 int main ()
 {
     string mystring;
-    mystring = "This is a string";
-    cout << mystring;
+    mystring = "This is the initial string content";
+    cout << mystring << endl;
+    mystring = "This is a different string content";
+    cout << mystring << endl;
     return 0;
 }
