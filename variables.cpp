109c6b5446b2903cac504f3999c23ff76ab4255f
Brad Richardson <brichardson@structint.com>
2016-12-22 07:20:01 -0800

    Different ways to initialize variables

--- variables.cpp
+++ variables.cpp
@@ -1,23 +1,18 @@
-// operating with variables
+// initialization of variables
 
 #include <iostream>
 using namespace std;
 
 int main ()
 {
-    // declaring variables;
-    int a, b;
-    int result;
+    int a=5;        // initial value: 5
+    int b(3);       // initial value: 3
+    int c{2};       // initial value: 2
+    int result;     // initial value undetermined
 
-    // process:
-    a = 5;
-    b = 2;
-    a = a + 1;
-    result = a - b;
-
-    // print out the result:
+    a = a + b;
+    result = a - c;
     cout << result;
 
-    // terminate the program:
     return 0;
 }
