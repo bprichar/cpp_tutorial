515ed505bbf7fdacbe9517167385d96fcfb1ae70
Brad Richardson <brichardson@structint.com>
2016-12-22 08:48:29 -0800

    Try out a for loop

--- loops.cpp
+++ loops.cpp
@@ -1,14 +1,11 @@
-// echo machine
+// countdown using a for loop
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main ()
 {
-    string str;
-    do {
-        cout << "Enter text: ";
-        getline(cin, str);
-        cout << "You entered: " << str << '\n';
-    } while (str != "goodbye");
+    for (int n = 10; n > 0; n--) {
+        cout << n << ", ";
+    }
+    cout << "liftoff!\n";
 }
